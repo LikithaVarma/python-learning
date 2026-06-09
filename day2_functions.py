bf752d008840f5d729de316cddc5464ac2ed4396
LikithaVarma <likithavarma112@gmail.com>
2026-06-09 11:43:36 +0530

    day 2 - functions, scope, default args, try/except

---
+++ day2_functions.py
@@ -0,0 +1,38 @@
+
+def add(num1,num2):
+    return num1+num2
+    
+print(add(2,3))
+
+x = 10
+
+def my_func():
+    global x
+    x = 99  
+    print(x)
+
+my_func()
+print(x)
+
+
+# day2_functions.py
+
+def calculate_grade(score):
+    try:
+        if score >= 90:
+            return "A"
+        elif score >= 80:
+            return "B"
+        elif score >= 70:
+            return "C"
+        else:
+            return "F"
+    except:
+        return "Invalid"
+
+
+# Test cases
+print("Score: 95 -> Grade:", calculate_grade(95))
+print("Score: 84 -> Grade:", calculate_grade(84))
+print("Score: 73 -> Grade:", calculate_grade(73))
+print("Score: 62 -> Grade:", calculate_grade("excellent"))
